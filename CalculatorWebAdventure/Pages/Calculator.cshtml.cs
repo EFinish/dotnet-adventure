887487e5f3ffbe5058c6dcbf6c5d9b8aa1ea87fe
EFinish <EFinish@gmail.com>
2024-01-17 07:00:19 +0100

    rename

--- CalculatorWebAdventure/Pages/Calculator.cshtml.cs
+++ CalculatorWebAdventure/Pages/Calculator.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CalculatorAdventure.Pages
+namespace CalculatorWebAdventure.Pages
 {
     public class CalculatorModel : PageModel
     {
